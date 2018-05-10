fd6b7e1270b53606c5f60c561baa54f01a64a5bc
bridge19 <ayong21st@163.com>
2018-05-10 19:16:42 +0800

    init

---
+++ kettle-example/src/main/java/com/xy/report/KettleRunner.java
@@ -0,0 +1,15 @@
+package com.xy.report;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class KettleRunner {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
